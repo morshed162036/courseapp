69649b70c5d572bfca2ab0b1c0d0addc8f9c13f2
Morshed Ahmed <mahmed162036@bscse.uiu.ac.bd>
2022-03-29 18:58:25 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -80,31 +80,100 @@
 // reportWebVitals();
 
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
 
- const fullName = 'Morshed Ahmed';
- const currDate = new Date().toLocaleDateString();
- const currTime = new Date().toLocaleTimeString();
- const img1 = "https://picsum.photos/200/300";
- const img2 = "https://picsum.photos/250/300";
- const img3 = "https://picsum.photos/300/300";
- const link = "https://youtube.com";
- ReactDOM.render(
-  <>
-    {/* <h1 contentEditable="true">My Name Is {fullName}</h1> */}
-    <p>Todays Date is {currDate}</p>
-    <p>Current Time is {currTime}</p>
-    <h1 className = "heading">My Name Is {fullName}</h1>
+//  const fullName = 'Morshed Ahmed';
+//  const currDate = new Date().toLocaleDateString();
+//  const currTime = new Date().toLocaleTimeString();
+//  const img1 = "https://picsum.photos/200/300";
+//  const img2 = "https://picsum.photos/250/300";
+//  const img3 = "https://picsum.photos/300/300";
+//  const link = "https://youtube.com";
+
+//  ReactDOM.render(
+//   <>
+//     {/* <h1 contentEditable="true">My Name Is {fullName}</h1> */}
+//     <p>Todays Date is {currDate}</p>
+//     <p>Current Time is {currTime}</p>
+//     <h1 className = "heading">My Name Is {fullName}</h1>
  
     
-    <div className='img_div'>
-    <img src={img1} alt="RandomImage"/>
-    <img src={img2} alt="RandomImage"/>
-    <a href={link} target="_parent">
-    <img src={img3} alt="RandomImage"/>
-    </a>
-    </div>
-  </>, document.getElementById("root")
- );
+//     <div className='img_div'>
+//     <img src={img1} alt="RandomImage"/>
+//     <img src={img2} alt="RandomImage"/>
+//     <a href={link} target="_parent">
+//     <img src={img3} alt="RandomImage"/>
+//     </a>
+//     </div>
+//   </>, document.getElementById("root")
+//  );
+
+//video=19
+// import React from 'react';
+// import  ReactDOM  from 'react-dom';
+
+
+// let currDate = new Date();
+// currDate = currDate.getHours();
+// let greetings = "";
+// let cssStyle = { };
+// if(currDate>=1 && currDate<12){
+//   greetings = 'Good Morning';
+//   cssStyle.color = 'green';
+// }else if(currDate>=12 && currDate<19){
+//   greetings = 'Good AfterNoon';
+//   cssStyle.color = 'red';
+// }else{
+//   greetings = 'Good Night';
+//   cssStyle.color = 'blue';
+// }
+
+// ReactDOM.render(
+//   <>
+//   <h1>Hello Sir, <span style = {cssStyle}>{greetings}</span></h1>
+//   </>, document.getElementById("root")
+// );
+
+//video=20
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// //import * as change from './Export_2'; // ei ta follow korle shob export data feach hoye jay jegular dorkaro ny.
+// import {greetings,cssStyle, myName} from './Export_2' // ei niyom follow korte hobe
+
+// ReactDOM.render(
+//   <>
+//    <h1>Hello Sir, <span style = {cssStyle}>{greetings}</span>, {myName()}</h1>
+//    {/* <h1>Hello Sir, <span style = {change.cssStyle}>{change.greetings}</span>, {change.myName()}</h1> */}
+//   </>, document.getElementById("root")
+// );
+
+// Video=21
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App'
+
+// ReactDOM.render(
+//   <App/>, document.getElementById('root')
+// );
+
+//video=22
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Card';
+
+ReactDOM.render(
+  <>
+  <Card imgsrc='https://wallpapercave.com/wp/wp4056410.jpg' title='A Netflix Original Series'
+    sname='DARK' link='https://www.netflix.com/in/title/80990668?source=35'
+  />
+  <Card imgsrc='https://wallpapercave.com/wp/wp1917154.jpg' title='A Netflix Original Series'
+    sname='Stranger Things' link='https://www.netflix.com/in/title/80990668?source=35'
+  />
+  <Card imgsrc='https://wallpapercave.com/wp/wp1917155.jpg' title='A Netflix Original Series'
+    sname='Extra Curricular' link='https://www.netflix.com/in/title/80990668?source=35'
+  />
+  </>, document.getElementById('root')
+)
